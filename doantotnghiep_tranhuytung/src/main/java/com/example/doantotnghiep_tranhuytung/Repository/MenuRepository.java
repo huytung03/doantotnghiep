71e1e9791d9a8d0698ac1dbe8f686ff03b74b926
huytung03 <tranhuytung987@gmail.com>
2025-02-27 10:35:09 +0700

    final

--- doantotnghiep_tranhuytung/src/main/java/com/example/doantotnghiep_tranhuytung/Repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.doantotnghiep_tranhuytung.Repository;
-
-import com.example.doantotnghiep_tranhuytung.Entity.MenuEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRepository extends JpaRepository<MenuEntity , Long> {
-}
--- doantotnghiep_tranhuytung/src/main/java/com/example/doantotnghiep_tranhuytung/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.doantotnghiep_tranhuytung.Service;
-
-import com.example.doantotnghiep_tranhuytung.Entity.UserEntity;
-
-public interface UserService {
-    UserEntity dangnhap (String email, String password);
-    void dangky(UserEntity userEntity);
-}
-
